b172ee721280dab53d252373cb423c5966015d6c
Diego Mogollón <diego.mogollon.camacho@gmail.com>
2022-11-15 20:17:41 -0500

    First commit course

---
+++ src/components/Characters.js
@@ -0,0 +1,45 @@
+export default function Characters(props){
+    const { characters, setCharacters } = props;
+    const resetCharacter =()=>{
+        setCharacters(null);
+    }
+    return(
+        <div className="characters">
+            <h1>Personajes</h1>
+            <span className="back-home" onClick={resetCharacter}>Volver al Inicio</span>
+            <div className="container-characters">
+                {characters.map((character,index)=>(
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name}></img>
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                    <span className="alive"/>
+                                    </>
+                                ):(
+                                    <>
+                                    <span className="dead"/>
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie: </span>
+                                <span>{character.species}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+
+            </div>
+            <span className="back-home" onClick={resetCharacter}>Volver al inicio</span>
+        </div>
+    );
+}
